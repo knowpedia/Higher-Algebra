3e4b97cf6a990363cc075dc5a9fefa1592a3e2e6
你好2007 <2501482523@qq.com>
2021-12-06 23:40:01 +0800

    调整代码结构（knowpedia作为quick-paper一个插件存在）

--- src/Service/mathFormula.js
+++ src/Service/mathFormula.js
@@ -1,18 +1,18 @@
 export default {
-    install(Knowpedia) {
+    install(QuickPaper) {
 
         // 矩阵Axy
-        Knowpedia.prototype.getMatrix = function (name, x, y,flag) {
+        QuickPaper.prototype.getMatrix = function (name, x, y, flag) {
             return this.$mathFormula.matrix([
                 [this.$mathFormula.rightBottom(name, 11), this.$mathFormula.rightBottom(name, 12), '...', this.$mathFormula.rightBottom(name, "1" + y)],
                 [this.$mathFormula.rightBottom(name, 21), this.$mathFormula.rightBottom(name, 22), '...', this.$mathFormula.rightBottom(name, "2" + y)],
                 ["...", "...", "", "..."],
                 [this.$mathFormula.rightBottom(name, x + "1"), this.$mathFormula.rightBottom(name, x + "2"), '...', this.$mathFormula.rightBottom(name, x + y)]
-            ],flag);
+            ], flag);
         };
 
         // 矩阵Axy的转置
-        Knowpedia.prototype.getMatrixT = function (name, x, y) {
+        QuickPaper.prototype.getMatrixT = function (name, x, y) {
             return this.$mathFormula.matrix([
                 [this.$mathFormula.rightBottom(name, 11), this.$mathFormula.rightBottom(name, 21), '...', this.$mathFormula.rightBottom(name, x + "1")],
                 [this.$mathFormula.rightBottom(name, 12), this.$mathFormula.rightBottom(name, 22), '...', this.$mathFormula.rightBottom(name, x + "2")],
@@ -22,7 +22,7 @@ export default {
         };
 
         // 矩阵(A+B)xy
-        Knowpedia.prototype.getAddMatrix = function (name1, name2, x, y) {
+        QuickPaper.prototype.getAddMatrix = function (name1, name2, x, y) {
             return this.$mathFormula.matrix([
                 [this.$mathFormula.join(this.$mathFormula.rightBottom(name1, 11), "+", this.$mathFormula.rightBottom(name2, 11)), this.$mathFormula.join(this.$mathFormula.rightBottom(name1, 12), "+", this.$mathFormula.rightBottom(name2, 12)), '...', this.$mathFormula.join(this.$mathFormula.rightBottom(name1, "1" + y), "+", this.$mathFormula.rightBottom(name2, "1" + y))],
                 [this.$mathFormula.join(this.$mathFormula.rightBottom(name1, 21), "+", this.$mathFormula.rightBottom(name2, 21)), this.$mathFormula.join(this.$mathFormula.rightBottom(name1, 22), "+", this.$mathFormula.rightBottom(name2, 22)), '...', this.$mathFormula.join(this.$mathFormula.rightBottom(name1, "2" + y), "+", this.$mathFormula.rightBottom(name2, "2" + y))],
@@ -32,12 +32,12 @@ export default {
         };
 
         // (aij)sn
-        Knowpedia.prototype.getlittleMatrix = function (a, ij, sn) {
+        QuickPaper.prototype.getlittleMatrix = function (a, ij, sn) {
             return this.$mathFormula.rightBottom(this.$mathFormula.bracket(this.$mathFormula.rightBottom(a, ij), 'small'), sn)
         };
 
         // (aij+bij)sn
-        Knowpedia.prototype.getAddlittleMatrix = function (a, b, ij, sn) {
+        QuickPaper.prototype.getAddlittleMatrix = function (a, b, ij, sn) {
             return this.$mathFormula.rightBottom(this.$mathFormula.bracket(this.$mathFormula.join(this.$mathFormula.rightBottom(a, ij), "+", this.$mathFormula.rightBottom(b, ij)), 'small'), sn);
         };
 
